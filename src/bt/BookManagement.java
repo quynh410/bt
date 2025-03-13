8400adca91a98e4da00fb2e96f6acd6b260b528a
quynh410 <quynhhhhhh07@gmail.com>
2025-03-13 10:15:46 +0700

    add_

--- src/bt/BookManagement.java
+++ src/bt/BookManagement.java
@@ -137,6 +137,33 @@ public class BookManagement {
                     System.out.println("Không tìm thấy sách của tác giả này.");
                 }
             }
+            public void searchByExport() {
+                System.out.print("Giá từ: ");
+                double start = scanner.nextDouble();
+                System.out.print("Đến: ");
+                double end = scanner.nextDouble();
+                scanner.nextLine();
+                for (int i = 0; i < bookCount; i++) {
+                    if (bookList[i].getExportPrice() >= start && bookList[i].getExportPrice() <= end) {
+                        bookList[i].displayData();
+                    }
+                }
+            }
+
+                public void bookStatistics() {
+                    System.out.print("Nhập tên tác giả: ");
+                    String authorName = scanner.nextLine();
+                    boolean found = false;
+                    for (int i = 0; i < bookCount; i++) {
+                        if (bookList[i].getAuthor().equalsIgnoreCase(authorName)) {
+                            bookList[i].displayData();
+                            found = true;
+                        }
+                    }
+                    if (!found) {
+                        System.out.println("Không tìm thấy sách của tác giả: " + authorName);
+                    }
+                }
         }
         BookManager manager = new BookManager();
         Scanner scanner = new Scanner(System.in);
@@ -151,7 +178,9 @@ public class BookManagement {
             System.out.println("5. Xóa sách");
             System.out.println("6. Sắp xếp sách theo lợi nhuận tăng dần");
             System.out.println("7. Tìm kiếm sách theo tác giả");
-            System.out.println("8. Thoát chương trình");
+            System.out.println("8. Tìm kiếm sách theo khoảng giá (tìm theo khoảng giá bán)");
+            System.out.println("9. Thống kê sách theo mỗi tác giả");
+            System.out.println("10. Thoát chương trình");
             System.out.print("Chọn một tùy chọn: ");
             int choice = scanner.nextInt();
             scanner.nextLine(); // Xử lý trôi lệnh
@@ -178,6 +207,10 @@ public class BookManagement {
                     manager.searchByAuthor();
                     break;
                 case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
                     System.out.println("Chương trình kết thúc.");
                     return;
                 default:
